f8d93031428e4a2dc4418c3ed73ae7524400ab0c
christophersulayao <christoper.sulayao@gmail.com>
2019-01-29 20:22:16 -0800

    test

--- search.js
+++ search.js
@@ -14,8 +14,8 @@ class Search {
 
     handleSearch () {
         var apiStuff = {
-            url: 'https://www.zomato.com/orange-county/drinks-and-nightlife-in-irvine',
-            // url: "https://danielpaschal.com/lfzproxies/yelpproxy.php?term=brewery&location=irvine",
+            // url: 'https://www.zomato.com/orange-county/drinks-and-nightlife-in-irvine',
+            url: "https://danielpaschal.com/lfzproxies/yelpproxy.php?term=restaurant&location=irvine",
             method: 'get',
             dataType: 'json',
             data: {
@@ -23,15 +23,15 @@ class Search {
                 location: 'irvine'
             },
             headers: {
-                apikey: '91cde05bc82f232037d522030640b69a'
-                // apikey: 'W65fNfobJPv2Ktfhu2f6Uox1BzTMceGzBuDu6ua7PAKXGJTx1oQjCHbuYuqDvLISCsCGl_9J4BUVYJMFmPxZordWAcP13859-LOZdYHu6s4D_x2xdvLXZmBJVLRPXHYx'
+                // apikey: '91cde05bc82f232037d522030640b69a'
+                apikey: 'W65fNfobJPv2Ktfhu2f6Uox1BzTMceGzBuDu6ua7PAKXGJTx1oQjCHbuYuqDvLISCsCGl_9J4BUVYJMFmPxZordWAcP13859-LOZdYHu6s4D_x2xdvLXZmBJVLRPXHYx'
             },
             success: functionCheck
         };
 
         function functionCheck(response) {
             console.log(response);
-            $('#ajax-response').text(JSON.stringify(response));
+            $('#ajax-response').text(JSON.stringify(response.name));
         }
 
         $('#ajax-response').text( );
